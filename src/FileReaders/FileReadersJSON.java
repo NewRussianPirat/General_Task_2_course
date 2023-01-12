f2e266de16eeb2d481e9d634d504003070db6389
NewRussianPirat <romaks1302@gmail.com>
2023-01-12 19:59:43 +0300

    Add readFile method in FileReadersJSON

--- src/FileReaders/FileReadersJSON.java
+++ src/FileReaders/FileReadersJSON.java
@@ -1,11 +1,36 @@
 package FileReaders;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FileReadersJSON implements FileReaders {
 
     @Override
     public ArrayList<String> readFile(String filename) {
-        return null;
+        try {
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(new FileReader(filename));
+            ArrayList<String> arrayList = new ArrayList<>();
+            JSONArray jsonArray = (JSONArray) jsonObject.get("expressions");
+            for (Object o : jsonArray) {
+                arrayList.add(o.toString());
+            }
+            return arrayList;
+        }
+        catch (FileNotFoundException e1) {
+            throw new RuntimeException("File not found");
+        }
+        catch (ParseException e2) {
+            throw new RuntimeException("ParseException");
+        }
+        catch (IOException e3) {
+            throw new RuntimeException("IOException");
+        }
     }
 }

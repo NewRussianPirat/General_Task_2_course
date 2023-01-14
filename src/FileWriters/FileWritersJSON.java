881e94d7a7a161b624a3899691c65bd20402d100
NewRussianPirat <romaks1302@gmail.com>
2023-01-15 01:37:20 +0300

    Add fileWrite method in FileWritersJSON

---
+++ src/FileWriters/FileWritersJSON.java
@@ -0,0 +1,47 @@
+package FileWriters;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWritersJSON extends FileWriters {
+
+    FileWriter fileWriter;
+    JSONArray jsonArray;
+
+    public FileWritersJSON() {
+        super();
+        fileWriter = null;
+        jsonArray = null;
+    }
+    public FileWritersJSON(String filename1) {
+        super(filename1);
+        try {
+            fileWriter = new FileWriter(filename1);
+            jsonArray = new JSONArray();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void writeFile(String string) {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("result", string);
+        jsonArray.add(jsonObject);
+    }
+
+    @Override
+    public void close() {
+        try {
+            fileWriter.write(jsonArray.toJSONString());
+            fileWriter.close();
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}

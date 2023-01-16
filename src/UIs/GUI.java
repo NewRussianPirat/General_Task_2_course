cd11930ea4d93b1114577c339a1f3f008b7b7b4d
NewRussianPirat <romaks1302@gmail.com>
2023-01-16 21:11:54 +0300

    GUI minor changes

--- src/UIs/GUI.java
+++ src/UIs/GUI.java
@@ -7,16 +7,21 @@ public class GUI {
 
     private static GUI gui = null;
 
-    private static final JFrame jFrame = new JFrame("Super Calculator");
+    private static final JFrame mainWindow = new JFrame();
+    private static final GUI_Panel mainPanel = new GUI_Panel();
+    private static final JLabel helloLabel = new JLabel(
+            "Welcome to my Super Calculator!",
+            SwingConstants.CENTER
+    );
 
-    private static final Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+    private static final Dimension SCREEN_SIZE = Toolkit.getDefaultToolkit().getScreenSize();
 
-    private static final int STANDARD_RIGHT_ANGLE_X = dimension.width / 2 - 600;
-    private static final int STANDARD_RIGHT_ANGLE_Y = dimension.height / 2 - 300;
+    private static final int STANDARD_RIGHT_ANGLE_X = SCREEN_SIZE.width / 2 - 600;
+    private static final int STANDARD_RIGHT_ANGLE_Y = SCREEN_SIZE.height / 2 - 300;
     private static final int STANDARD_WIDTH = 1200;
     private static final int STANDARD_HEIGHT = 600;
 
-    private static final int MINIMUM_WIDTH = 1200;
+    private static final int MINIMUM_WIDTH  = 1200;
     private static final int MINIMUM_HEIGHT = 600;
 
     public static GUI getInstance() {
@@ -30,13 +35,36 @@ public class GUI {
 
     public void createGUI() {
         setDefaultWindowSettings();
+        helloLabel.setVisible(true);
+        addAll(
+                helloLabel,
+                mainPanel
+        );
     }
 
     private void setDefaultWindowSettings() {
-        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        jFrame.setBounds(STANDARD_RIGHT_ANGLE_X, STANDARD_RIGHT_ANGLE_Y, STANDARD_WIDTH, STANDARD_HEIGHT);
-        jFrame.setMinimumSize(new Dimension(MINIMUM_WIDTH, MINIMUM_HEIGHT));
-        jFrame.setVisible(true);
+        mainWindow.setTitle("Super Calculator");
+        mainWindow.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        mainWindow.setBounds(STANDARD_RIGHT_ANGLE_X, STANDARD_RIGHT_ANGLE_Y, STANDARD_WIDTH, STANDARD_HEIGHT);
+        mainWindow.setMinimumSize(new Dimension(MINIMUM_WIDTH, MINIMUM_HEIGHT));
+        mainWindow.setVisible(true);
     }
 
+    @SafeVarargs
+    private static <T extends JComponent> void addAll(T ... args) {
+        for (var jComponent : args) {
+            mainWindow.add(jComponent);
+        }
+    }
+
+    private static class GUI_Panel extends JPanel {
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            Graphics2D graphics2D = (Graphics2D) g;
+            this.setBounds(0, 0, mainWindow.getWidth(), mainWindow.getHeight());
+            helloLabel.setBounds(this.getWidth() / 2 - 300, 50 , 600, 100);
+            this.setLayout(null);
+        }
+    }
 }
